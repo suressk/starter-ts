782da6092bd193880c5c7c34b7be5dc8cced281b
suressk <sure_ssk@outlook.com>
2022-07-16 12:25:35 +0800

    chore: update `sure-utils`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { isNull } from 'sure-utils'
+
 export const one = 4
 export const two = 2
 
@@ -16,9 +18,12 @@ const help = () => {
   // unused vars 'error'
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [a, _, c] = use(1, 2, 3)
+
   // don't recommend 'console.xx' func
   // eslint-disable-next-line no-console
   console.log('count: %d', a)
+
+  const _res = isNull(a)
 }
 
 help()
